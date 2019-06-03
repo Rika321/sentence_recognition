c21fe70e1cf5974869878bf980db58572ba2e6ed
Rika321 <quhe1996@gmail.com>
2019-06-03 00:25:57 -0700

    naveBar fixed

--- text_classifier/static/text_classifier/js/utility.move.js
+++ text_classifier/static/text_classifier/js/utility.move.js
@@ -19,39 +19,38 @@ function getColorByBaiFenBiMove(bili){
     return "rgb("+r+","+g+","+b+")";
 }
 
-  var sent_conf = parseInt(document.getElementById("sent_conf").value*100);
+function drawResult(sent_conf) {
+    sent_conf = parseInt(sent_conf*100);
+    console.log(sent_conf);
+    var elem1 = document.getElementById("myBar1");
+    var elem2 = document.getElementById("myBar2");
+    elem1.style.backgroundColor = getColorByBaiFenBiMove(100-sent_conf);
+    elem2.style.backgroundColor = getColorByBaiFenBiMove(sent_conf);
+    var width1 = 0;
+    var width2 = 0;
+    var id1 = setInterval(frame1, 450/(sent_conf));
+    var id2 = setInterval(frame2, 450/(100-sent_conf));
 
-  var elem1 = document.getElementById("myBar1");
-  var elem2 = document.getElementById("myBar2");
-  elem1.style.backgroundColor = getColorByBaiFenBiMove(sent_conf);
-  elem2.style.backgroundColor = getColorByBaiFenBiMove(100-sent_conf);
-  // document.getElementById("myProgress1").style.color = getColorByBaiFenBiMove(sent_conf);
-  // document.getElementById("myProgress2").style.color = getColorByBaiFenBiMove(100-sent_conf);
-
-
-
-  var width1 = 0;
-  var width2 = 0;
-  var id1 = setInterval(frame1, 450/(sent_conf));
-  var id2 = setInterval(frame2, 450/(100-sent_conf));
-
-  function frame1() {
-    if (width1 >= (sent_conf)) {
-      clearInterval(id1);
+    function frame1() {
+      if (width1 >= (sent_conf)) {
+        clearInterval(id1);
+      }
+      else {
+        width1++;
+        elem1.style.width = width1 + '%';
+        elem1.innerHTML = width1 * 1  + '%';
+      }
     }
-    else {
-      width1++;
-      elem1.style.width = width1 + '%';
-      elem1.innerHTML = width1 * 1  + '%';
+    function frame2() {
+      if (width2 >= (100-sent_conf)) {
+        clearInterval(id2);
+      }
+      else {
+        width2++;
+        elem2.style.width = width2 + '%';
+        elem2.innerHTML = width2 * 1  + '%';
+      }
     }
-  }
-  function frame2() {
-    if (width2 >= (100-sent_conf)) {
-      clearInterval(id2);
-    }
-    else {
-      width2++;
-      elem2.style.width = width2 + '%';
-      elem2.innerHTML = width2 * 1  + '%';
-    }
-  }
+}
+  // var sent_conf = parseInt(document.getElementById("sent_conf").value*100);
+  // console.log(sent_conf);
